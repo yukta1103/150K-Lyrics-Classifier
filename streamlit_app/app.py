4dd6afb48e5b76d9a350948cfa104967928a2e2d
anjuvilashni <anjuvilashni.n@gmail.com>
2025-11-22 10:26:45 -0500

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import os
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import joblib
 import numpy as np
 from src.preprocess import simple_tokenize
